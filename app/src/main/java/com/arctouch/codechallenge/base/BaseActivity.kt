1800f5d4de77d2fcde2ffcfef78af2c30e3f8cf9
Gustavo Pasqualini <pasqualini.gustavo@gmail.com>
2019-05-19 12:14:20 -0300

    Deprecated progressdialog

--- app/src/main/java/com/arctouch/codechallenge/base/BaseActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/base/BaseActivity.kt
@@ -1,16 +1,13 @@
 package com.arctouch.codechallenge.base
 
-import android.app.ProgressDialog
-import android.content.Context
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.Theme
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.base.interfaces.ActivityBehaviour
 import com.arctouch.codechallenge.di.components.AppComponent
-import com.arctouch.codechallenge.di.modules.ActivityModule
 import java.util.*
 
 abstract class BaseActivity : AppCompatActivity, ActivityBehaviour {
@@ -19,33 +16,16 @@ abstract class BaseActivity : AppCompatActivity, ActivityBehaviour {
     protected val appComponent: AppComponent
         get() = (application as ArchtouchApplication).getApplicationComponent()
 
-    protected val activityModule: ActivityModule
-        get() = ActivityModule(this)
-
     private var tryToClose: Date? = null
     private var blockOnBackPress = false
     protected var layoutId: Int = 0
-    protected var viewContentId: Int = 0
-    private var inputManager: InputMethodManager? = null
-    protected var progressDialog: ProgressDialog? = null
+    protected var progressDialog: MaterialDialog? = null
     protected var questionBeforeClose = true
-
-    constructor(layoutId: Int, viewContentId: Int) : super() {
-        this.TAG = this.javaClass.simpleName
-
-        this.layoutId = layoutId
-        this.viewContentId = viewContentId
-    }
 
     constructor(layoutId: Int) : super() {
         this.TAG = this.javaClass.simpleName
 
         this.layoutId = layoutId
-        this.viewContentId = 0
-    }
-
-    override fun backFragment() {
-        this.onBackPressed()
     }
 
     protected abstract fun initComponents()
@@ -63,10 +43,6 @@ abstract class BaseActivity : AppCompatActivity, ActivityBehaviour {
         this.init(savedInstanceState)
     }
 
-    /**
-     * Method responsible to call all init method.
-     * If you need to change the sequence/behavior of all init call override this method
-     */
     protected fun init(savedInstanceState: Bundle?) {
         this.blockOnBackPress = false
         this.injectComponents()
@@ -75,70 +51,19 @@ abstract class BaseActivity : AppCompatActivity, ActivityBehaviour {
         this.initData()
     }
 
-    override fun switchContent(fragment: Fragment, addBackStack: Boolean) {
-        this.switchContent(fragment, this.viewContentId, addBackStack)
-    }
-
-    override fun switchContent(fragment: Fragment, idRes: Int, addToBackStack: Boolean) {
-        val transaction = this.supportFragmentManager.beginTransaction()
-        val tag = fragment.javaClass.canonicalName
-        transaction.replace(idRes, fragment, tag)
-        if (addToBackStack) {
-            transaction.addToBackStack(tag)
-        }
-        transaction.commit()
-    }
-
-    override fun clearFragmentStack() {
-        while (this.hasBackStack()) {
-            this.supportFragmentManager.popBackStackImmediate()
-        }
-    }
-
-    override fun activityEffectTransition(enterAnim: Int, exitAnim: Int) {
-        this.overridePendingTransition(enterAnim, exitAnim)
-    }
-
     protected fun hasBackStack(): Boolean {
         return this.supportFragmentManager.backStackEntryCount > 0
     }
 
-    override fun hideKeyboard() {
-        if (this.inputManager == null) {
-            this.inputManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        }
-
-        if (this.currentFocus != null && this.currentFocus!!.windowToken != null) {
-            this.inputManager!!.hideSoftInputFromWindow(
-                    this.currentFocus!!.windowToken,
-                    InputMethodManager.HIDE_NOT_ALWAYS
-            )
-        }
-    }
-
-    override fun showKeyboard() {
-        if (this.inputManager == null) {
-            this.inputManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        }
-
-        if (this.currentFocus != null && this.currentFocus!!.windowToken != null) {
-            this.inputManager!!.showSoftInputFromInputMethod(
-                    this.currentFocus!!.windowToken,
-                    InputMethodManager.HIDE_NOT_ALWAYS
-            )
-        }
-    }
-
     override fun showProgressBar() {
         if (progressDialog == null || !progressDialog!!.isShowing) {
-            val dialog = ProgressDialog(this)
-
-            dialog.setMessage(getString(R.string.label_please_wait))
-            dialog.isIndeterminate = true
-            dialog.setCancelable(false)
-            dialog.show()
-
-            progressDialog = dialog
+            val b = MaterialDialog.Builder(this)
+            b.title(R.string.app_name)
+            b.cancelable(false)
+            b.theme(Theme.LIGHT)
+            b.content(R.string.label_please_wait)
+            b.progress(true, 0)
+            progressDialog = b.show()
         }
     }
 
--- app/src/main/java/com/arctouch/codechallenge/base/interfaces/ActivityBehaviour.kt
+++ app/src/main/java/com/arctouch/codechallenge/base/interfaces/ActivityBehaviour.kt
@@ -1,26 +1,9 @@
 package com.arctouch.codechallenge.base.interfaces
 
-import androidx.fragment.app.Fragment
-
 interface ActivityBehaviour {
-
-    fun switchContent(fragment: Fragment, addToBackStack: Boolean)
-
-    fun switchContent(fragment: Fragment, idRes: Int, animation: Boolean)
-
-    fun clearFragmentStack()
-
-    fun activityEffectTransition(enterAnim: Int, exitAnim: Int)
-
-    fun hideKeyboard()
-
-    fun showKeyboard()
 
     fun showProgressBar()
 
     fun hideProgressBar()
-
-    fun backFragment()
-
 }
 
